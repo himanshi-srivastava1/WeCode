7a35a3394189f16040f47959ac43639e137363c2
himanshi-srivastava1 <himansri04@gmail.com>
2026-05-23 00:52:26 +0530

    fix: resolve invalid project id bug in socket joining logic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,13 +16,13 @@ export default defineConfig({
   },
   server: {
     headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Embedder-Policy': 'credentialless',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
   preview: {
     headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Embedder-Policy': 'credentialless',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },
